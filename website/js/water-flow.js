d19170f86f6447ea1d9a7299eebd1fc24dfa3fde
mcleanka <mcleankasambala@gmail.com>
2022-11-09 01:58:50 +0200

    Fix:: debugging

--- website/js/water-flow.js
+++ website/js/water-flow.js
@@ -1,7 +1,7 @@
 const waterFlowUrl = APP_URL + "/ffhms/list?action=waterFlow"
 
 const waterFlowEle = new Chart(document.getElementById("water-flow-chart"), {
-  type: 'line',
+  type: 'area',
   data: {
     labels: [],
     datasets: [{
