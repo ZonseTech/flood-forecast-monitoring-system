b6a96a5bebae9532551019be05cbc6de117529d3
mcleanka <mcleankasambala@gmail.com>
2022-11-09 01:41:55 +0200

    Fix:: debugging

--- website/js/temperature.js
+++ website/js/temperature.js
@@ -60,7 +60,7 @@ function formatAMPM(date) {
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
   minutes = minutes < 10 ? '0' + minutes : minutes;
-  var strTime = minutes + ':' + hours + ' ' + ampm;
+  var strTime = minutes + ':' + seconds + ' ' + ampm;
   return strTime;
 }
 
