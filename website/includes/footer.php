a4454d23614c8af59fd4e42e7376aa7527684e15
mcleanka <mcleankasambala@gmail.com>
2022-11-09 01:13:39 +0200

    Fix:: debugging

--- website/includes/footer.php
+++ website/includes/footer.php
@@ -34,10 +34,9 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/export-data.js"></script>
-<script src="https://code.highcharts.com/modules/accessibility.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+
 
 <script>
 var APP_URL = "http://52.201.237.65:81/api"
--- website/js/temperature.js
+++ website/js/temperature.js
@@ -1,165 +1,55 @@
-const temperatureEle = document.getElementById("temperature-chart");
 const temperatureUrl = APP_URL + "/ffhms/list?action=temperature"
 
-$(document).ready(function () {
-  var temperatureEle = {
-    type: 'spline',
-    animation: Highcharts.svg, // don't animate in IE < IE 10.
-    marginRight: 10,
-
-    events: {
-      load: function () {
-        // set up the updating of the chart each second
-        var series = this.series[0];
-        var temperature;
-
-        setInterval(async function () {
-          await axios.get(temperatureUrl)
-            .then((response) => response.data)
-            .then((result) => {
-              console.log(result);
-              temperature = result.data
-            })
-
-          console.log(temperature);
-
-          var x = (new Date()).getTime(), // current time
-            y = Math.random();
-          series.addPoint([x, temperature], true, true);
-        }, 5000);
-      }
-    }
-  };
-  var title = {
-    text: ''
-  };
-  var xAxis = {
-    type: 'datetime',
-    tickPixelInterval: 150
-  };
-  var yAxis = {
-    title: {
-      text: 'Value'
-    },
-    plotLines: [{
-      value: 0,
-      width: 1,
-      color: '#808080'
+const ctx_live = document.getElementById("temperature-chart");
+const temperatureEle = new Chart(ctx_live, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      data: [],
+      borderWidth: 1,
+      borderColor: '#006699',
+      label: 'Bin Distance',
     }]
-  };
-  var tooltip = {
-    formatter: function () {
-      return '<b>' + this.series.name + '</b><br/>' +
-        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-        Highcharts.numberFormat(this.y, 2);
-    }
-  };
-  var plotOptions = {
-    area: {
-      pointStart: 1940,
-      marker: {
-        enabled: false,
-        symbol: 'circle',
-        radius: 2,
-
-        states: {
-          hover: {
-            enabled: true
-          }
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "Trash Accumulation",
+    },
+    legend: {
+      display: false
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
         }
-      }
+      }]
     }
-  };
-  var legend = {
-    enabled: false
-  };
-  var exporting = {
-    enabled: false
-  };
-  var series = [{
-    name: 'Random data',
-    data: (function () {
-      // generate an array of random data
-      var data = [], time = (new Date()).getTime(), i, temperature = 0;
-
-      axios.get(temperatureUrl)
-        .then((response) => response.data)
-        .then((result) => temperature = result.data.value)
-
-      for (i = -19; i <= 0; i += 1) {
-        data.push({
-          x: time + i * 1000,
-          y: temperature
-        });
-      }
-
-      return data;
-    }())
-  }];
-
-  var json = {};
-  json.chart = temperatureEle;
-  json.title = title;
-  json.tooltip = tooltip;
-  json.xAxis = xAxis;
-  json.yAxis = yAxis;
-  json.legend = legend;
-  json.exporting = exporting;
-  json.series = series;
-  json.plotOptions = plotOptions;
-
-  Highcharts.setOptions({
-    global: {
-      useUTC: false
-    }
-  });
-
-  $('#temperature-chart').highcharts(json);
+  }
 });
 
-/* if (temperatureEle) {
-  var temperatureChart = Highcharts.chart("temperature-chart", {
-    chart: {
-      type: 'spline'
-    },
-    title: {
-      text: ''
-    },
-    xAxis: {
-      title: {
-        text: 'Time (Hrs)'
-      },
-      categories: [],
-    },
-    yAxis: {
-      title: {
-        text: 'Degrees Celcius'
-      },
-      labels: {
-        formatter: function () {
-          return this.value + '°';
-        }
-      }
-    },
-    tooltip: {
-      crosshairs: true,
-      shared: true
-    },
-    plotOptions: {
-      spline: {
-        marker: {
-          radius: 4,
-          lineColor: '#666666',
-          lineWidth: 1
-        }
-      }
-    },
-    series: []
+const getData = function () {
+  $.ajax({
+    url: temperatureUrl,
+    success: (data) => {
+      time = (new Date()).getTime()
+
+      temperatureEle.data
+        .labels
+        .push(time);
+
+      temperatureEle.data
+        .datasets[0]
+        .data
+        .push(data.data);
+
+      temperatureEle.update();
+
+    }
   });
+};
 
- 
-
-  setInterval(() => {
-    
-  }, 5000)
-} */
+setInterval(getData, 10000);
