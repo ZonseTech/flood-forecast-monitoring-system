183f523a890f05cff30f03b698e9049ba5e0e7d2
mcleanka <mcleankasambala@gmail.com>
2022-11-09 01:06:23 +0200

    Fix:: debugging

--- website/js/temperature.js
+++ website/js/temperature.js
@@ -13,10 +13,13 @@ $(document).ready(function () {
         var series = this.series[0];
         var temperature;
 
-        setInterval(function () {
-          axios.get(temperatureUrl)
+        setInterval(async function () {
+          await axios.get(temperatureUrl)
             .then((response) => response.data)
-            .then((result) => temperature = result.data)
+            .then((result) => {
+              console.log(result);
+              temperature = result.data
+            })
 
           var x = (new Date()).getTime(), // current time
             y = Math.random();
