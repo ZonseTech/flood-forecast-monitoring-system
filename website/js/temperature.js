9510ff4bf9ef959875a8c05ad6a5f94048987e6f
mcleanka <mcleankasambala@gmail.com>
2022-11-09 00:58:10 +0200

    Fix:: debugging

--- website/js/temperature.js
+++ website/js/temperature.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
           var x = (new Date()).getTime(), // current time
             y = Math.random();
           series.addPoint([x, y], true, true);
-        }, 8000);
+        }, 1000);
       }
     }
   };
