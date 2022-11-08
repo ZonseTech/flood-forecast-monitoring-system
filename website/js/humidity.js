1cd98653d87b5f29ef0a53f7dd4f3a0c245d221f
mcleanka <mcleankasambala@gmail.com>
2022-11-08 22:56:48 +0200

    Fix:: debugging

--- website/js/humidity.js
+++ website/js/humidity.js
@@ -1,56 +1,56 @@
 const humidityEle = document.getElementById("humidity-chart");
 
-if(humidityEle) {
-    var humidityChart = Highcharts.chart("humidity-chart", {
-        chart: {
-          type: 'spline'
-        },
-        title: {
-          text: ''
-        },
-        xAxis: {
-          title: {
-            text: 'Time (Hrs)'
-        },
-          categories: ['8am', '9am', '10am', '11am', '12pm', '1pm',
-            '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-        },
-        yAxis: {
-          title: {
-            text: 'Percentage'
-          },
-          labels: {
-            formatter: function () {
-              return this.value + '%';
-            }
-          }
-        },
-        tooltip: {
-          crosshairs: true,
-          shared: true
-        },
-        plotOptions: {
-          spline: {
-            marker: {
-              radius: 4,
-              lineColor: '#666666',
-              lineWidth: 1
-            }
-          }
-        },
-        series: []
-      });
-    
-    const humidityUrl = "http://localhost/flood-forecast/website/api.php?action=list&type=humidity"
-    
-    setInterval(()=>{
+if (humidityEle) {
+  var humidityChart = Highcharts.chart("humidity-chart", {
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: ''
+    },
+    xAxis: {
+      title: {
+        text: 'Time (Hrs)'
+      },
+      categories: ['8am', '9am', '10am', '11am', '12pm', '1pm',
+        '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+    },
+    yAxis: {
+      title: {
+        text: 'Percentage'
+      },
+      labels: {
+        formatter: function () {
+          return this.value + '%';
+        }
+      }
+    },
+    tooltip: {
+      crosshairs: true,
+      shared: true
+    },
+    plotOptions: {
+      spline: {
+        marker: {
+          radius: 4,
+          lineColor: '#666666',
+          lineWidth: 1
+        }
+      }
+    },
+    series: []
+  });
+
+  const humidityUrl = APP_URL + "/api.php?action=list&type=humidity"
+
+  setInterval(() => {
     axios.post(humidityUrl, {})
-    .then((response)=> response.data)
-    .then((data)=>humidityChart.updateSeries([
+      .then((response) => response.data)
+      .then((data) => humidityChart.updateSeries([
         {
-            name: 'Humidity',
-            data,
-    }.catch((e)=>e.message)
-    ]))
-    }, 1000)
+          name: 'Humidity',
+          data,
+        }.catch((e) => e.message)
+      ]))
+  }, 1000)
 }
