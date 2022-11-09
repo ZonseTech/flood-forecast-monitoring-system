3643fc37b6e28bdac0d06b6bc5f58f49ffceaa38
mcleanka <mcleankasambala@gmail.com>
2022-11-09 02:10:27 +0200

    Fix:: debugging

--- website/index.php
+++ website/index.php
@@ -1,138 +1,137 @@
-<?php include("./includes/header.php") ?>
+<?php include "./includes/header.php"?>
 
-    <!-- Begin Page Content -->
-    <div class="container-fluid">
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-        <!-- Page Heading -->
-        <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-danger shadow-sm"><i
-                    class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-        </div>
+	<!-- Page Heading -->
+	<div class="d-sm-flex align-items-center justify-content-between mb-4">
+		<h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+		<a href="#" class="d-none d-sm-inline-block btn btn-sm btn-danger shadow-sm"><i
+				class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+	</div>
 
-        <!-- Content Row -->
-        <div class="row">
+	<!-- Content Row -->
+	<div class="row">
 
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-danger shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">     
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Temperature
-                                </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">27 <sup>o</sup>C</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-temperature-high fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+		<!-- Earnings (Monthly) Card Example -->
+		<div class="col-xl-3 col-md-6 mb-4">
+			<div class="card border-left-danger shadow h-100 py-2">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+								Temperature
+							</div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800">27 <sup>o</sup>C</div>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-temperature-high fa-2x text-gray-300"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-danger shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Humidity
-                                </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">75%</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-humidity"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+		<!-- Earnings (Monthly) Card Example -->
+		<div class="col-xl-3 col-md-6 mb-4">
+			<div class="card border-left-danger shadow h-100 py-2">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+								Humidity
+							</div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800">75%</div>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-humidity"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-danger shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Water Level
-                                </div>
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col-auto">
-                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">225 M</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-water fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+		<!-- Earnings (Monthly) Card Example -->
+		<div class="col-xl-3 col-md-6 mb-4">
+			<div class="card border-left-danger shadow h-100 py-2">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+								Water Level
+							</div>
+							<div class="row no-gutters align-items-center">
+								<div class="col-auto">
+									<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">225 M</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-water fa-2x text-gray-300"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-            <!-- Pending Requests Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-danger shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Water Flow
-                                </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">2 ltrs/min</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-water fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+		<!-- Pending Requests Card Example -->
+		<div class="col-xl-3 col-md-6 mb-4">
+			<div class="card border-left-danger shadow h-100 py-2">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+								Water Flow
+							</div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800">2 ltrs/min</div>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-water fa-2x text-gray-300"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-        <!-- Content Row -->
+	<!-- Content Row -->
 
-        <div class="row">
+	<div class="row">
 
-            <!-- Area Chart -->
-            <div class="col-xl-8 col-lg-7">
-                <div class="card shadow mb-4">
-                    <!-- Card Header - Dropdown -->
-                    <div
-                        class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">
-                            Water Level
-                        </h6>
-                    </div>
-                    <!-- Card Body -->
-                    <figure class="card-body">
-                        <div id="water-level-chart"></div>
-                    </figure>
-                </div>
-            </div>
+		<!-- Area Chart -->
+		<div class="col-xl-8 col-lg-7">
+			<div class="card shadow mb-4">
+				<!-- Card Header - Dropdown -->
+				<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+					<h6 class="m-0 font-weight-bold text-primary">
+						Water Level
+					</h6>
+				</div>
+				<!-- Card Body -->
+				<div class="card-body">
+					<canvas id="water-level-chart"></canvas>
+				</div>
+			</div>
+		</div>
 
-            <!-- Pie Chart -->
-            <div class="col-xl-4 col-lg-5">
-                <div class="card shadow my-0 mx-0">
-                    <!-- Card Header - Dropdown -->
-                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Calendar</h6>
-                    </div>
-                    <!-- Card Body -->
-                    <div class="card-body">
-                        <div class="chart-pie pt-4 pb-2">
-                            <div id="bootstrap-datepicker"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+		<!-- Pie Chart -->
+		<div class="col-xl-4 col-lg-5">
+			<div class="card shadow my-0 mx-0">
+				<!-- Card Header - Dropdown -->
+				<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+					<h6 class="m-0 font-weight-bold text-primary">Calendar</h6>
+				</div>
+				<!-- Card Body -->
+				<div class="card-body">
+					<div class="chart-pie pt-4 pb-2">
+						<div id="bootstrap-datepicker"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-    </div>
-    <!-- /.container-fluid -->
-  
-<?php include("./includes/footer.php") ?>
+</div>
+<!-- /.container-fluid -->
+
+<?php include "./includes/footer.php"?>
